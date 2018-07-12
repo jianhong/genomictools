aac2c43fce72d9dfbd15de078fee15eb6896f346
Jianhong Ou <jianhongou@dhcp-152-16-223-142.research.duhs.duke.edu>
2018-07-12 15:42:36 -0400

    remove BSgenome packages.

--- install.R
+++ install.R
@@ -1,3 +1,3 @@
 source("https://bioconductor.org/biocLite.R")
-biocLite(c("ChIPpeakAnno", "trackViewer", "motifStack", "ATACseqQC", "GeneNetworkBuilder", "BSgenome.Hsapiens.UCSC.hg38", "TxDb.Hsapiens.UCSC.hg38.knownGene", "org.Hs.eg.db", "BSgenome.Drerio.UCSC.danRer10", "TxDb.Drerio.UCSC.danRer10.refGene", "org.Dr.eg.db"), suppressUpdates=TRUE, ask=FALSE)
+biocLite(c("ChIPpeakAnno", "trackViewer", "motifStack", "ATACseqQC", "GeneNetworkBuilder", "TxDb.Hsapiens.UCSC.hg38.knownGene", "org.Hs.eg.db", "TxDb.Drerio.UCSC.danRer10.refGene", "org.Dr.eg.db"), suppressUpdates=TRUE, ask=FALSE)
 
